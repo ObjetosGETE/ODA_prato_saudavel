7a2473136522f6d9a80d1131ab3fb2a2e62f74af
luiz-brancher <lppbrancher@senacrs.com.br>
2023-11-09 18:46:29 -0300

    + javascript

--- assets/js/prato_saudavel.js
+++ assets/js/prato_saudavel.js
@@ -4,22 +4,39 @@ $('.start_btn').click(function () {
         $('.tela-2').fadeIn()
     }, 500)
 })
+
 // Lógica do Objeto:
 let selected = 0;
-
+let corretos = 0;
+let errados = 0;
 
 $('.verificar_btn').click(function () {
     let telaAtual = $(this).parents('.tela')
     let proxTela = $(this).parents('.tela').next('.tela')
-    telaAtual.fadeOut()
-    setTimeout(() => {
-        proxTela.fadeIn()
-    }, 500)
+    let selecionadosArray = $('.selected')
+    console.log(selecionadosArray)
+    for(let i=0; i<selecionadosArray.length;i++){
+        if (selecionadosArray[i].classList.contains('cr')) {
+            corretos++
+        } else {
+            errados++
+        }
+    };
+    if (errados > 0){
+        console.log('whoops, try again!')
+    }
+    if (errados == 0){
+        telaAtual.fadeOut()
+        setTimeout(() => {
+            proxTela.fadeIn()
+        }, 500)
+    }
+
 })
 
 $('.g-item').click(function () {
     let item = $(this)
-    if(item.hasClass('selected')){
+    if (item.hasClass('selected')) {
         item.removeClass('selected')
         selected--
         console.log('selected sqrs: ' + selected)
@@ -28,7 +45,4 @@ $('.g-item').click(function () {
         selected++
         console.log('selected sqrs: ' + selected)
     }
-})
-$('.verificar_btn').click(function(){
-    
 })
