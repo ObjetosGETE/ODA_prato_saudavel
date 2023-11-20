32843f6af3dbe526e063ef6abf00156797eb6fec
luiz-brancher <lppbrancher@senacrs.com.br>
2023-11-20 17:38:35 -0300

    logica do resultado?

--- assets/js/prato_saudavel.js
+++ assets/js/prato_saudavel.js
@@ -12,15 +12,15 @@ $('.start_btn').click(function () {
 
 // Lógica do Objeto:
 let selected = 0;
-
+let corretos = 0
 $('.verificar_btn').click(function () {
     let telaAtual = $(this).parents('.tela')
     let proxTela = $(this).parents('.tela').next('.tela')
     let selecionadosArray = $('.selected')
-    let corretos = 0
+    
     let errados = 0
     console.log(selecionadosArray)
-    for(let i=0; i<selecionadosArray.length;i++){
+    for (let i = 0; i < selecionadosArray.length; i++) {
         if (selecionadosArray[i].classList.contains('cr')) {
             corretos++
         } else {
@@ -29,18 +29,17 @@ $('.verificar_btn').click(function () {
     };
     console.log('errados: ' + errados)
     console.log('corretos: ' + corretos)
-    if (errados > 0){
-        console.log('whoops, try again!')
-    }
-    if (errados == 0 && selecionadosArray.length == 4){
-        console.log(selecionadosArray.length)
-        telaAtual.fadeOut()
-        setTimeout(() => {
-            proxTela.fadeIn()
-        }, 500)
-        $('.selected').removeClass('selected')
-    }
+    console.log(selecionadosArray.length)
+    telaAtual.fadeOut()
+    setTimeout(() => {
+        proxTela.fadeIn()
+    }, 500)
+    $('.selected').removeClass('selected')
+    if(telaAtual.hasClass('tela-4')){
+        // resultado => (selecionados/corretos)*100
+        $('.pontuacao span').text((corretos/16)*100)
 
+    }
 })
 
 $('.g-item').click(function () {
@@ -56,10 +55,9 @@ $('.g-item').click(function () {
     }
 })
 
-$('.ingredientes_btn').click(function(){
+$('.ingredientes_btn').click(function () {
     $(this).parents('.tela').children('.section-2').children('.modal_ingredientes').fadeIn()
 })
-$('.modal_cls_btn').click(function(){
+$('.modal_cls_btn').click(function () {
     $(this).parents('.modal_ingredientes').fadeOut()
 })
-// resultado => (selecionados/corretos)*100
