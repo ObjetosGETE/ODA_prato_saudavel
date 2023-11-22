e0022af4feb144e23a0af0ae65f7e57c2a240de0
luiz-brancher <lppbrancher@senacrs.com.br>
2023-11-22 10:16:08 -0300

    modal resultado individual pra cada tela

--- assets/js/prato_saudavel.js
+++ assets/js/prato_saudavel.js
@@ -2,6 +2,7 @@ $('.tela-3').hide()
 $('.tela-4').hide()
 $('.tela-5').hide()
 $('.modal_ingredientes').hide()
+$('.modal_resultado').hide()
 
 $('.start_btn').click(function () {
     $('.tela-1').fadeOut();
@@ -15,8 +16,9 @@ let selected = 0;
 let corretos = 0
 $('.verificar_btn').click(function () {
     let telaAtual = $(this).parents('.tela')
-    let proxTela = $(this).parents('.tela').next('.tela')
+    let modal = telaAtual.children('.modal_resultado')
     let selecionadosArray = $('.selected')
+    
     
     let errados = 0
     console.log(selecionadosArray)
@@ -27,9 +29,16 @@ $('.verificar_btn').click(function () {
             errados++
         }
     };
+    modal.children('.points').children('span').text(corretos)
+    modal.fadeIn()
     console.log('errados: ' + errados)
     console.log('corretos: ' + corretos)
     console.log(selecionadosArray.length)
+    
+})
+$('.next_btn').click(function(){
+    let telaAtual = $(this).parents('.tela')
+    let proxTela = $(this).parents('.tela').next('.tela')
     telaAtual.fadeOut()
     setTimeout(() => {
         proxTela.fadeIn()
@@ -38,7 +47,6 @@ $('.verificar_btn').click(function () {
     if(telaAtual.hasClass('tela-4')){
         // resultado => (selecionados/corretos)*100
         $('.pontuacao span').text((corretos/16)*100)
-
     }
 })
 
@@ -56,7 +64,7 @@ $('.g-item').click(function () {
 })
 
 $('.ingredientes_btn').click(function () {
-    $(this).parents('.tela').children('.section-2').children('.modal_ingredientes').fadeIn()
+    $(this).parents('.tela').children('.modal_ingredientes').fadeIn()
 })
 $('.modal_cls_btn').click(function () {
     $(this).parents('.modal_ingredientes').fadeOut()
